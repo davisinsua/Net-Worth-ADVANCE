5ffc98866ad03b789d9ac2f6d0e7d0bc44edf62f
jguadagno7555 <82001925+jguadagno7555@users.noreply.github.com>
2021-04-05 19:31:35 -0400

    Add files via upload

---
+++ CalcFormWindowController.java
@@ -0,0 +1,132 @@
+package CalcForm;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class CalcFormWindowController {
+
+    @FXML
+    private Button exitButtonID;
+
+    @FXML
+    private TextArea transBox1;
+
+    @FXML
+    private TextArea transBox2;
+
+    @FXML
+    private TextArea transBox3;
+
+    @FXML
+    private TextArea transBox4;
+
+    @FXML
+    private TextArea transBox5;
+
+    @FXML
+    private TextArea transBox6;
+
+    @FXML
+    private TextArea transBox7;
+
+    @FXML
+    private TextArea transBox8;
+
+    @FXML
+    private TextArea transBox9;
+
+    @FXML
+    private TextArea transBox10;
+
+    @FXML
+    private Button vButtonID1;
+
+    @FXML
+    private Button vButtonID2;
+
+    @FXML
+    private Button vButtonID3;
+
+    @FXML
+    private Button vButtonID4;
+
+    @FXML
+    private Button vButtonID5;
+
+    @FXML
+    private Button vButtonID6;
+
+    @FXML
+    private Button vButtonID7;
+
+    @FXML
+    private Button vButtonID8;
+
+    @FXML
+    private Button vButtonID9;
+
+    @FXML
+    private Button vButtonID10;
+
+    @FXML
+    void exitButtonClicked(ActionEvent event) {
+        Stage stage = (Stage) exitButtonID.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void vButton1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vButton10(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vButton2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vButton3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vButton4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vButton5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vButton6(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vButton7(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vButton8(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vButton9(ActionEvent event) {
+
+    }
+
+}
+
+
+
