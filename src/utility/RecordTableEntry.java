c603fe5bf9448f9dd6a217f8911b405340d51b21
henry-512 <77709977+henry-512@users.noreply.github.com>
2021-04-09 17:01:54 -0400

    Create RecordTableEntry.java
    This is required to make the record window display its table values properly.

---
+++ src/utility/RecordTableEntry.java
@@ -0,0 +1,31 @@
+package utility;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * Cursed wrapper class to make JavaFX tables function properly. Check
+ * RecordWindow for usage.
+ * 
+ * @author Henry
+ *
+ */
+public class RecordTableEntry {
+    public StringProperty type;
+    public DoubleProperty value;
+
+    public RecordTableEntry(String type, Double value) {
+        this.type = new SimpleStringProperty(type);
+        this.value = new SimpleDoubleProperty(value);
+    }
+
+    public String getType() {
+        return type.get();
+    }
+
+    public Double getValue() {
+        return value.get();
+    }
+}
