5698fba0d8500a42bd1cfeeee3b9accb45326ede
Skittlez21 <jaredx291@gmail.com>
2021-04-17 13:00:40 -0400

    Update GraphWindow and add libraries

--- src/windows/GraphWindow.java
+++ src/windows/GraphWindow.java
@@ -1,8 +1,22 @@
 // Net Worth ADVANCE
 // Class GraphWindow
+package src.windows;
 
-public class GraphWindow extends JFrame
-{
+import javafx.scene.text.Font;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.RefineryUtilities;
+import utility.UserRecord;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Arrays;
+
+public class GraphWindow extends JFrame {
 
     private static final long serialVersionUID = 1L;
 
@@ -11,54 +25,45 @@ public class GraphWindow extends JFrame
         // Create dataset
         JFreeChart lineChart = ChartFactory.createLineChart(
                 "Net Worth over time",
-                "Date","Net Worth",
+                "Date", "Net Worth",
                 createDataset(),
                 PlotOrientation.VERTICAL,
-                true,true,false);
-        ChartPanel chartPanel = new ChartPanel( lineChart );
-        chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
-        setContentPane( chartPanel );
+                true, true, false);
+        ChartPanel chartPanel = new ChartPanel(lineChart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(560, 367));
+        setContentPane(chartPanel);
     }
 
     private DefaultCategoryDataset createDataset() {
 
         String series1 = "Net Worth";
 
+        ArrayList<UserRecord> userRecords;
+        userRecords = new ArrayList<>();
+
+
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-
-        dataset.addValue(100*Math.random(), series1, "2016-12-19");
-        dataset.addValue(150*Math.random(), series1, "2016-12-20");
-        dataset.addValue(100*Math.random(), series1, "2016-12-21");
-        dataset.addValue(210*Math.random(), series1, "2016-12-22");
-        dataset.addValue(240*Math.random(), series1, "2016-12-23");
-        dataset.addValue(195*Math.random(), series1, "2016-12-24");
-        dataset.addValue(245*Math.random(), series1, "2016-12-25");
-
+        for (String f : Font.getFamilies()) {
+            utility.UserRecord r = utility.UserRecord.createNewRecord(f);
+            for (UserRecord.FiscalEntryType t : UserRecord.FiscalEntryType.values()) {
+                r.addEntry(t, t.isAsset ? 100 * Math.random() : -100 * Math.random());
+            }
+            userRecords.add(r);
+        }
+        userRecords.listIterator().forEachRemaining(rec -> {
+            String date = rec.getDateRecorded();
+            double worth = rec.getNetWorth();
+            dataset.addValue(worth, series1, date);
+        });
         return dataset;
     }
 
     public static void main(String[] args) {
-            GraphWindow example = new GraphWindow("Line Chart Example");
-            example.pack();
-            example.setSize(600, 400);
-            RefineryUtilities.centerFrameOnScreen( example );
-            example.setVisible(true);
+        GraphWindow example = new GraphWindow("Net Worth Over time");
+        example.pack();
+        example.setSize(600, 400);
+        RefineryUtilities.centerFrameOnScreen(example);
+        example.setVisible(true);
 
     }
-
-
-
-    public void createGraph()
-   {
-
-   }
-
-   public void displayGraph()
-   {
-
-   }
-
-   public void saveGraphing()
-   {
-
-   }
+}
