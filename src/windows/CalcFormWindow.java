d5b70bc76a24ae1e396240afb9fb395a5c4cb6a2
davisinsua <64988214+davisinsua@users.noreply.github.com>
2021-04-19 20:28:12 -0400

    Integrated CalcFormWindow to MainWindow

--- src/windows/CalcFormWindow.java
+++ src/windows/CalcFormWindow.java
@@ -18,15 +18,7 @@ import javafx.scene.text.*;
 import utility.UserRecord;
 import org.w3c.dom.Node;
 
-/* TODO:
-   Bugtest
-   Gui streamline - add subheaders to pages. sub headers would be like "Cash and Cash Equivalent Assets" for page 1.
-   clean up code & comment sections   
-   Maybe add "stylize" function for labels to avoid repeated lines of code for the font and color change
-   read enum names instead of string for pages
-*/
-
-public class CalcFormWindow extends Application // extends Stage for Window display
+public class CalcFormWindow extends Stage // extends Stage for Window display
 {   
    /* Create datafields for the window interface */
    // Root - parent VBox that holds every element
@@ -53,8 +45,7 @@ public class CalcFormWindow extends Application // extends Stage for Window disp
    // pages - arraylist that contains the string arrays that contain the fiscalLabels for each respective page
    ArrayList<String[]> pages = new ArrayList<String[]>();
    
-   // String arrays for each page - the subheader is the last element always
-   // Look into getting enum names rather than hard coding
+   /* String arrays for each page */   
    // Cash and Cash Equivalent Assets
    String page1[] = {"Checking Acounts", "Savings Acounts", "Money Market Acounts",
     "Savings bonds", "CD's", "Cash Value of Life Insurance"};
@@ -89,12 +80,8 @@ public class CalcFormWindow extends Application // extends Stage for Window disp
    // Create Record for this calcualation sequence
    UserRecord record = UserRecord.createNewRecord("temp");
    
-   public static void main(String[] args) { // Remove this when calling from CalcFormWindow
-      launch(args);
-   }
-   
-   @Override // Make this constructor when running to call this from mainWindow, ex. public CalcFormWindow()
-   public void start(Stage myStage) {   
+   // Constructor for CalcFormWindow
+   public CalcFormWindow() {   
       
       // Add page strings to page ArrayList
       pages.add(page1);
@@ -122,14 +109,17 @@ public class CalcFormWindow extends Application // extends Stage for Window disp
       root.setStyle("-fx-background-color: black;");
       
       // Add action for next button
-      // It will call the updatePage() function
-      // and move to the next page.
+      // It will call the updatePage() function and move to the next page.
       bttnNext.setOnAction(e->updatePage());
+      
+      // Action for save button
       save.setOnAction((event) -> {
-            System.out.println("Saving...");
-            if (saveText.getText() == "")
-               saveText.setText("default");
-               
+         System.out.println("Saving...");
+         
+         // If no name entered, make it default
+         if (saveText.getText() == "")
+            saveText.setText("default");
+         
          record.setRecordName(saveText.getText());
          record.writeRecordToFile();
          System.out.println("Saved!");
@@ -145,21 +135,19 @@ public class CalcFormWindow extends Application // extends Stage for Window disp
       calcFormWindow.show();
    }
      
-  	/**
-	 * Function to move to the next page of net worth entries.
-	 * Will update UserRecord object with entries.
+  /**
+    * Function to move to the next page of net worth entries.
+    * Will update UserRecord object with entries.
     *
     * @author Davis
-	 */
+    */
   public void updatePage() {
-  
-      try {
-         if (pageNum != 0)
-            saveEntries();
-      }
       
+      // Call saveEntries function the first time the user presses the next buton.
+      try { if (pageNum != 0) saveEntries(); }
+      
+      // Catch when user enters in non-digit entries
       catch (NumberFormatException e) {
-         // make pop up window if time
          System.out.println("Please only enter digits!");
          return;
       } 
@@ -196,13 +184,13 @@ public class CalcFormWindow extends Application // extends Stage for Window disp
          fiscalEntryLabel.setTextFill(Color.WHITE);
          
          // create new textfield to enter value, add a negative sign for liability pages
-         TextField amount = new TextField((pageNum > 3) ? "-" : "");
+         TextField amount = new TextField((pageNum > 3) ? "-0.0" : "0.0");
          
          // create a new HBox and add it to entryRows ArrayList
          entryRows.add(new HBox());
          // entryRows aesthetic modifications
          entryRows.get(i).setAlignment(Pos.CENTER);
-         entryRows.get(i).setSpacing(15);
+         entryRows.get(i).setSpacing(250);
          entryRows.get(i).getChildren().addAll(fiscalEntryLabel, amount);
          
          // Add the newly created and populated entryRow to the root         
@@ -217,18 +205,18 @@ public class CalcFormWindow extends Application // extends Stage for Window disp
   }   
   
   /**
-	 * Function to save the input entries to the UserRecord object generated at the start of the sequence.
+    * Function to save the input entries to the UserRecord object generated at the start of the sequence.
     *
     * @author Davis
-	 */
+    */
    public void saveEntries() {
       // Loop through every entry row on current page
       for (int i = 0; i < pages.get(pageNum - 1).length; i++) {
-         // Change empty boxes to 0.0 value
+         // Change empty and boxes to 0.0 value: prevent exception
          if (((TextField) entryRows.get(i).getChildren().get(1)).getText() == "")
             ((TextField) entryRows.get(i).getChildren().get(1)).setText("0.0");
             
-         // Add entry from this entry row
+         // Add entry from this entry row's textfield in the most java way possible
          record.addEntry(UserRecord.FiscalEntryType.values()[enumCounter], Double.parseDouble(((TextField) entryRows.get(i).getChildren().get(1)).getText()));
          System.out.println("Added value: "+record.getEntryByType(UserRecord.FiscalEntryType.values()[enumCounter])+" to enum: "+UserRecord.FiscalEntryType.values()[enumCounter]);
          enumCounter++;
@@ -236,14 +224,14 @@ public class CalcFormWindow extends Application // extends Stage for Window disp
    }  
   
   /**
-	 * Function to calculate the net worth from passed UserRecord object.
-	 * @param type The UserRecord to calculate the net worth for.
-	 * @return The calculated net worth as a double value.
+    * Function to calculate the net worth from passed UserRecord object.
+    * @param type The UserRecord to calculate the net worth for.
+    * @return The calculated net worth as a double value.
     *
     * @author Davis
-	 */
+    */
   public double calcNetW(UserRecord record) {
- 
+  
       double netWorth = 0;
       
       // Iterate over all type enums, add them to netWorth
@@ -253,37 +241,36 @@ public class CalcFormWindow extends Application // extends Stage for Window disp
       }
       
       System.out.println("Calculated Net Worth = "+netWorth);
-      
       return netWorth;
   }
   
   /**
-	 * Function to display the calculated networth to the user.
+    * Function to display the calculated networth to the user.
     * The final page of the form.
     *
     * @author Davis
-	 */
+    */
     public void displayResult() {
-    
       // Clear page
       clearPage();
       
       // Update net worth value in record
       record.setNetWorth(calcNetW(record));
       
-      // Update headers
+      // Update final page labels
       header = new Label("Congratulations!");
       header.setFont(Font.font("Consolas", FontWeight.BOLD, 70));
       header.setTextFill(Color.WHITE);
+      
       subHeader = new Label("Your calculated Net Worth is: "+record.getNetWorth());
       subHeader.setFont(Font.font("Consolas", FontWeight.NORMAL, 30));
       subHeader.setTextFill(Color.WHITE);
-      
-      // Make HBox for save button and text
+
       Label saveLabel = new Label("Enter name for record and save ->  ");
       saveLabel.setFont(Font.font("Consolas", FontWeight.NORMAL, 15));
       saveLabel.setTextFill(Color.WHITE);
       
+      // Make HBox for save button and text
       HBox saveBox = new HBox();
       saveBox.getChildren().addAll(saveLabel, saveText, save);
       saveBox.setAlignment(Pos.CENTER);
@@ -293,10 +280,10 @@ public class CalcFormWindow extends Application // extends Stage for Window disp
   }
   
   /**
-	 * Helper function to clear the page of current elements.
+    * Helper function to clear the page of current elements.
     *
     * @author Davis
-	 */
+    */
     public void clearPage() {
       root.getChildren().clear();
       headerBox.getChildren().clear();
